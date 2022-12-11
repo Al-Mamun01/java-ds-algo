1ee89a282f218e6e17c447f4572a2e79743be24f
mamun272 <mdmamunkhanshaheb@gmail.com>
2022-12-11 14:28:14 +0600

    two variable of sum

---
+++ main.java
@@ -0,0 +1,17 @@
+
+
+
+
+import java.util.*;
+public class main{
+    public static void main (String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter Your first Number ");
+        int number1=sc.nextInt();
+        System.out.println("Enter your second Number ");
+        int number2=sc.nextInt();
+        int sum =number1+number2;
+        System.out.println("your Number is sum");
+        System.out.println(sum);
+    }
+}
---
+++ mm.kt
@@ -0,0 +1,3 @@
+fun mainA(){
+  print("hello world");
+}
